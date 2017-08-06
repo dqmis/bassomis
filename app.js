55f9c566f3876ddc1f025a60a7a99904bb952560
Dominykas Seputis <dom.seputis@gmail.com>
2017-08-06 20:59:17 +0300

    Commiting

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const port = process.env.PORT || 8080;
 
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/', (req, res) => {
